9fb1a259c0c739d475778733c8481acb060c303d
Renato <renatosouzacruz263@gmail.com>
2025-07-22 00:21:27 -0300

    refactor: add 'nav-link' class to navbar links for styling purposes

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
         <!-- ESTILOS O PROJETO -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
     <!-- GOOGLE FONTS --><link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -21,10 +21,10 @@
        </a>
        <nav>
         <ul id="navbar">
-            <li><a href="<?= $BASE_URL ?>">Home</a></li>
-            <li><a href="#">Categorias</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="<?= $BASE_URL ?>contato.php">contato</a></li>
+            <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+            <li><a href="#" class="nav-link">Categorias</a></li>
+            <li><a href="#" class="nav-link">Sobre</a></li>
+            <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">contato</a></li>
         </ul>
        </nav>
     </header>
